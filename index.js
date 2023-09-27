13f9feb56023025f43d90e23cf52ecfeba18c3cf
hritikdubey-22 <hritik.dubey@polynomial.a>
2023-09-27 19:30:14 +0530

    port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const port = process.env.PORT;
+const port = process.env.PORT || 7777;
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
